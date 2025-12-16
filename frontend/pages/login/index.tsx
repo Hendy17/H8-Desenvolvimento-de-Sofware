cbc6c88a3b5240e58beeb88d87478e94bbc94a0d
hendy <hendyvorpagel@gmail.com>
2025-12-16 19:02:45 -0300

    upadate code

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -1,3 +1,23 @@
-import LoginPage from './login';
+import { Layout } from 'antd';
+import LoginForm from '../../components/login';
+import Link from 'next/link';
 
-export default LoginPage;
+const { Header, Content } = Layout;
+
+export default function Login() {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header style={{ background: '#001529', padding: '0 24px' }}>
+        <div style={{ color: '#fff', fontWeight: 600 }}>H8 Desenvolvimento de software</div>
+      </Header>
+      <Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div>
+          <LoginForm />
+          <div style={{ textAlign: 'center', marginTop: 12 }}>
+            <Link href="/register">Criar nova conta</Link>
+          </div>
+        </div>
+      </Content>
+    </Layout>
+  );
+}
